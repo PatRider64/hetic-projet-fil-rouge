f9edfe2f7202dbde4ef0d3703f943c08b43f9ae0
Leila042 <pavilla.leila@gmail.com>
2023-09-03 22:50:24 +0200

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+
+// export default function A () {
+//   return (
+//     <BrowserRouter>
+//        <Routes>
+//        <Route path="/" element={<Home />} />
+//       </Routes>
+//     </BrowserRouter>
+ 
+
+//   );
+
+// };
 
 function App() {
   return (
+
+
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,10 +34,12 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      
     </div>
   );
-}
+};
 
 export default App;
+  // cours, connexion, compte, à propos, page tarif > paiement, page où les instruments sont répertoriés
