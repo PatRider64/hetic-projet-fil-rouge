795a76bde1081860b9687b15b558cf1c8c107b1e
Leila042 <pavilla.leila@gmail.com>
2023-09-16 03:14:37 +0200

    Faq

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Button} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import 'tailwindcss/tailwind.css'; 
